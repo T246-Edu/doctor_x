46cd9c436db95ae0eb014da3408d2c2467288312
T246-Edu <tawfik.2419010@stemdakahlia.moe.edu.eg>
2022-02-09 17:39:11 +0200

    Updates

---
+++ precache-manifest.28172f3cd6f0d233b6b0d7e4e6b23017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07f4737d5b6339e68075",
+    "url": "/doctor_x/static/css/main.00735c25.chunk.css"
+  },
+  {
+    "revision": "07f4737d5b6339e68075",
+    "url": "/doctor_x/static/js/main.059c7892.chunk.js"
+  },
+  {
+    "revision": "cf95c315e9fd04d966fb",
+    "url": "/doctor_x/static/js/runtime~main.2cf206cd.js"
+  },
+  {
+    "revision": "2e56b4a8ce93cfa7b62f",
+    "url": "/doctor_x/static/css/2.09fc6bc6.chunk.css"
+  },
+  {
+    "revision": "2e56b4a8ce93cfa7b62f",
+    "url": "/doctor_x/static/js/2.952a52cc.chunk.js"
+  },
+  {
+    "revision": "b0356833c44f3e7812d999a6368b50b5",
+    "url": "/doctor_x/index.html"
+  }
+];
